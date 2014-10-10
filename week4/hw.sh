db6a1f17bdae25ec38253ed52936e188d5f4e799
RyamJuthan <x00088236@ittd.ie>
2014-10-10 10:24:19 +0100

    week4

---
+++ week4/hw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#: Title : Question 3 hello world
+#: Date : 10/10/2014
+#: Author : Ryan Juthan 
+#: Version : 1
+#: Description : Week4
+#: Options : N/A
+
+printf "%s\n" "Hello, World!" 
