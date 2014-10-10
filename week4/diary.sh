4c9c3e348cebfc4c058680078d76158950392c2c
RyanJuthan <x00088236@ittd.ie>
2014-10-10 11:58:43 +0100

    changes

---
+++ week4/diary.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#: Title : question 6 
+#: Date : 10/10/14
+#: Author : Ryan Juthan 
+#: Version : 1
+#: Description : week 4 
+#: Options : 
+
+printf "Please add entry to diary "
+read input_variable1
+$input_varibale1 >> diary.txt
+
