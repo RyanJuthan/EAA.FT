e72fb9b319cb0e646a78980d46f90484ad5d63c0
RyanJuthan <x00088236@ittd.ie>
2014-10-24 11:57:26 +0100

    changes

---
+++ week6/CPUCount.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#: Title : Prcocessor 
+#: Date : 03/10/2014
+#: Author : Ryan Juthan 
+#: Version :1
+#: Description : counts the numbers of processors 
+#: Options :
+
+printf "the number of avaibale processors is: " >> CPU.txt
+
+grep processor /proc/cpuinfo | wc -l >> CPU.txt
+
